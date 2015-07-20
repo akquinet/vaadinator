69fa24d1dba65d3a05338d1dfbd1793c5babd9b7
unknown <srothbucher@NB-204398.akquinet.de>
2015-07-20 22:20:01 +0200

    Correct theming issue in the AddressbookExample

--- VaadinatorExample/AddressbookExample/src/main/java/de/akquinet/engineering/vaadinator/example/address/AddressbookExampleUIEx.java
+++ VaadinatorExample/AddressbookExample/src/main/java/de/akquinet/engineering/vaadinator/example/address/AddressbookExampleUIEx.java
@@ -1,13 +1,24 @@
 package de.akquinet.engineering.vaadinator.example.address;
 
+import com.vaadin.addon.touchkit.server.TouchKitServlet;
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 
-@Theme("touchkitexex")
 public class AddressbookExampleUIEx extends AddressbookExampleUI {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// for mobile, set yet another theme
+		if (VaadinServlet.getCurrent() instanceof TouchKitServlet) {
+			setTheme("touchkitexex");
+		}
+		super.init(request);
+	}
 
 }
