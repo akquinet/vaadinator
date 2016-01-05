139e609ad62c988bcc99f27fb330ac3932c4aa27
Daniel Nordhoff-Vergien <dve@vergien.net>
2016-01-05 10:31:46 +0100

    Not working implementation of modularization with SPI

---
+++ VaadinatorTooling/VaadinatorGeneratorTestedit/src/main/java/org/vergien/TestCodeGenerator.java
@@ -0,0 +1,15 @@
+package org.vergien;
+
+import de.akquinet.engineering.vaadinator.generator.CodeGenerator;
+import de.akquinet.engineering.vaadinator.mojo.VaadinatorConfig;
+
+public class TestCodeGenerator implements CodeGenerator {
+
+	@Override
+	public void generateCode(VaadinatorConfig vaadinatorConfig) throws Exception {
+
+		throw new Exception("Not yet implemented");
+
+	}
+
+}
