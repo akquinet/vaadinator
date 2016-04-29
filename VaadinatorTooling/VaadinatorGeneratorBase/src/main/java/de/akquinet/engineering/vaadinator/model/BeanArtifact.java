377b811c16bf33c590c6c99a512c2c7b86ac0626
Daniel Nordhoff-Vergien <dve@vergien.net>
2016-04-29 14:11:51 +0200

    Add enum for generated artifacts per bean

---
+++ VaadinatorTooling/VaadinatorGeneratorBase/src/main/java/de/akquinet/engineering/vaadinator/model/BeanArtifact.java
@@ -0,0 +1,11 @@
+package de.akquinet.engineering.vaadinator.model;
+
+/**
+ * Artifacts which are generated per bean.
+ * 
+ * @author dve
+ *
+ */
+public enum BeanArtifact {
+	LIST_VIEW_IMPL, LIST_VIEW, LIST_PRES, LIST_PRES_IMPL, EDIT_VIEW_IMPL, EDIT_VIEW, EDIT_PRES, EDIT_PRES_IMPL, ADD_VIEW, ADD_VIEW_IMPL, ADD_PRES, ADD_PRES_IMPL, CHANGE_VIEW, CHANGE_VIEW_IMP, CHANGE_PRES, CHANGE_PRES_IMPL
+}
