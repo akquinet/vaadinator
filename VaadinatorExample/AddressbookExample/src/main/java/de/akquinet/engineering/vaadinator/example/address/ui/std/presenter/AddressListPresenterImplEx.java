46a49e2cc48221e646436785d85cc3b2bc479e6a
Daniel Nordhoff-Vergien <dve@vergien.net>
2016-12-19 14:00:23 +0100

    #31 - fix presenter in example to only load data once

--- VaadinatorExample/AddressbookExample/src/main/java/de/akquinet/engineering/vaadinator/example/address/ui/std/presenter/AddressListPresenterImplEx.java
+++ VaadinatorExample/AddressbookExample/src/main/java/de/akquinet/engineering/vaadinator/example/address/ui/std/presenter/AddressListPresenterImplEx.java
@@ -6,18 +6,24 @@ import de.akquinet.engineering.vaadinator.example.address.service.AddressService
 import de.akquinet.engineering.vaadinator.example.address.ui.presenter.SubviewCapablePresenter;
 import de.akquinet.engineering.vaadinator.example.address.ui.std.view.AddressListView;
 
-public class AddressListPresenterImplEx extends AddressListPresenterImpl{
+public class AddressListPresenterImplEx extends AddressListPresenterImpl {
 
 	public AddressListPresenterImplEx(Map<String, Object> context, AddressListView view,
 			PresenterFactory presenterFactory, AddressService service,
 			SubviewCapablePresenter subviewCapablePresenter) {
 		super(context, view, presenterFactory, service, subviewCapablePresenter);
-		
+
 	}
 
 	@Override
 	protected void loadFromModel() {
-		// TODO Auto-generated method stub
-		super.loadFromModel();
+		getView().setOrRefreshData(null);
 	}
+
+	@Override
+	public void startPresenting() {
+		getView().setObserver(this);
+		getView().initializeUi();
+	}
+
 }
